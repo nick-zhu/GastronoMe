e602509425a3f8de59eeabd4d39aaa1a93ce4c1d
Nick <hanzhang.zhu@hds.com>
2018-02-18 12:03:16 -0800

    skelenton code

---
+++ server.js
@@ -0,0 +1,22 @@
+import express from 'express';
+import morgan from 'morgan';
+import bobyParser from 'body-parser';
+
+const app = express();
+const router = express.Router();
+const port = process.env.PORT || 8000;
+
+// app.use(morgan('combined'))
+app.use(express.static(__dirname + '/dist'));
+
+router.get('/', (req, res) => {
+  console.log(`listening on port ${port}`);
+  
+  console.log(req.query.form_data);
+  res.json(JSON.parse(req.query.form_data));
+})
+
+app.use('/recipes', router);
+
+app.listen(port);
+console.log(`listening on port ${port}`);
